53f42fdb41d14b2e9b32d31135c0067f376b9611
Hatzi2 <jonas.lind10@gmail.com>
2025-04-04 09:07:19 +0200

    srv now works with ÆØÅ

--- backend/srv.py
+++ backend/srv.py
@@ -1,3 +1,4 @@
+﻿# -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask import send_from_directory
@@ -6,26 +7,22 @@ import json
 import os
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
 CORS(app)
 
 @app.route('/pdf/<path:filename>')
 def serve_pdf(filename):
-    # Go one level up from the backend folder to reach the project root,
-    # then navigate to Files/policer-Raw.
     pdf_dir = os.path.join(os.path.dirname(app.root_path), 'Files', 'policer-Raw')
     return send_from_directory(pdf_dir, filename)
-
 
 @app.route("/run-script", methods=["POST"])
 def run_script():
     data = request.get_json()
     filename = data.get("filename")
-
     if not filename:
         return jsonify({"error": "Filename not provided"}), 400
 
     output_path = f"Files/Output/{os.path.splitext(filename)[0]}.json"
-
     if not os.path.exists(output_path):
         subprocess.run(["python", "backend/main.py", filename])
 
@@ -36,13 +33,11 @@ def run_script():
     else:
         return jsonify({"error": "Output file not found"}), 500
 
-
 @app.route("/list-files", methods=["GET"])
 def list_files():
     folder_path = "Files/policer-Raw"
     if not os.path.exists(folder_path):
         return jsonify({"error": "Folder not found"}), 404
-
     file_list = os.listdir(folder_path)
     return jsonify(file_list)
 
@@ -58,8 +53,8 @@ def get_progress():
     output_path = f"Files/Output/{os.path.splitext(filename)[0]}.json"
     if os.path.exists(output_path):
         return jsonify({
-            "ocr": {"progress": 1.0, "status": "Scanning Finitto"},
-            "main": {"progress": 1.0, "status": "finitto"}
+            "ocr": {"progress": 1.0, "status": "Scanning Færdig"},
+            "main": {"progress": 1.0, "status": "Færdig"}
         })
 
     progress_file = os.path.join("Files", "ProgressBar", "progress.json")
@@ -78,7 +73,6 @@ def get_progress():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
 @app.route("/reset-progress", methods=["POST"])
 def reset_progress():
     progress_file = os.path.join("Files", "ProgressBar", "progress.json")
@@ -86,10 +80,9 @@ def reset_progress():
     with open(progress_file, "w", encoding="utf-8") as f:
         json.dump({
             "ocr": {"progress": 0.0, "status": "Starter scanning..."},
-            "main": {"progress": 0.0, "status": "YEP"}
-        }, f)
+            "main": {"progress": 0.0, "status": "Venter på scanning..."}
+        }, f, ensure_ascii=False)
     return jsonify({"status": "reset"}), 200
-
 
 if __name__ == "__main__":
     app.run(port=5000)
